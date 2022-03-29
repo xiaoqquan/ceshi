101dc2556d549def046494bf72a803de259c8ec3
xiaoqquan <1597718727@qq.com>
2022-03-29 18:13:34 +0800

    8563783

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
   {
     private static readonly string[] Summaries = new[]
     {
-            "aFreezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "aFreezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "你好Balmy", "Hot", "Sweltering", "Scorching"
         };
 
     private readonly ILogger<WeatherForecastController> _logger;
